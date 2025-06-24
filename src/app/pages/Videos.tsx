0cdd3e59a9b35a54001b98bfe5a33be50ca73f99
jjuniorbrasil <juniorbrasilisc@gmail.com>
2025-06-24 14:36:13 -0300

    feat: add pj video

--- src/app/pages/Videos.tsx
+++ src/app/pages/Videos.tsx
@@ -4,7 +4,7 @@ export function Videos() {
   return (
     <section
       id="videos"
-      className="@container flex flex-col md:flex-row min-h-screen align-middle justify-center px-8 py-16 gap-8 md:w-7/8 mx-auto snap-center"
+      className="@container flex flex-col lg:flex-row min-h-screen align-middle justify-center px-8 py-16 gap-8 md:w-7/8 mx-auto snap-center"
     >
       <div className="content-center flex-1">
         <h1 className="text-main-lighter text-[250%] font-normal dark:font-light dark:font-lighter">
@@ -27,6 +27,20 @@ export function Videos() {
         <div className="aspect-video flex-1 @max-w-1/3 shadow-black/100 shadow-2xl">
           <iframe
             src="https://www.youtube.com/embed/VH-XSRFzQW4?si=GJu7XC-yzGis54lR"
+            title="Tutorial de Utilização"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="w-full h-full rounded-2xl"
+          />
+        </div>
+      </div>
+      <div className="content-center flex-1">
+        <h1 className="text-main-lighter text-[250%] font-normal dark:font-light dark:font-lighter">
+          Plug-in de vídeos
+        </h1>
+        <div className="aspect-video flex-1 @max-w-1/3 shadow-black/100 shadow-2xl">
+          <iframe
+            src="https://www.youtube.com/embed/5SkfxnXbDtI?si=_-WETx7kE8eaGptK"
             title="Tutorial de Utilização"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
